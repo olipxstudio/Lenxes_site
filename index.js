8f2173339e595d125d40372ed07a6ba402a11dad
olipxstudio <102828903+olipxstudio@users.noreply.github.com>
2022-05-16 23:52:20 +0100

    Node Server

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+const host_name = process.env.HOST
+const user_name = process.env.USER
+const password_name = process.env.PASSWORD
+const database_name = process.env.DATABASE
+const port = process.env.SERVER_PORT || 4000
+
+
+const db = mysql.createConnection({
+    host: host_name,
+    user: 'root',
+    password: password_name,
+    database: database_name
+})
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
