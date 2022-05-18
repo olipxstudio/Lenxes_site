5ed0a71e4d53ca0e0002eb1719e77866c59dc244
MagyzTech <60434675+MagyzTech@users.noreply.github.com>
2022-05-18 11:11:42 +0100

    Hey philip! I'm just testing my first commit

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql2')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+const app = express();
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
-const host_name = process.env.HOST
-const user_name = process.env.USER
-const password_name = process.env.PASSWORD
-const database_name = process.env.DATABASE
-const port = process.env.SERVER_PORT || 4000
-
+const host_name = process.env.HOST;
+const user_name = process.env.USER;
+const password_name = process.env.PASSWORD;
+const database_name = process.env.DATABASE;
+const port = process.env.SERVER_PORT || 4000;
 
 const db = mysql.createConnection({
-    host: host_name,
-    user: 'root',
-    password: password_name,
-    database: database_name
-})
+  host: host_name,
+  user: "root",
+  password: password_name,
+  database: database_name,
+});
 
-app.get('/', (req, res) => {
-    res.status(200).send('Welcome')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome");
+});
+
+// Omega just cloned and made hs first commit
+app.get("/omega", (req, res) => {
+  res.status(200).send("Omega is here. One Love <3.");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port http://localhost:${port}`);
+});
