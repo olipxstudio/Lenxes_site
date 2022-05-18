e0e78e567cec10b6460bce19243657af9f190d73
olipxstudio <102828903+olipxstudio@users.noreply.github.com>
2022-05-18 21:34:41 +0100

    routes and controllers

--- index.js
+++ index.js
@@ -25,47 +25,31 @@ app.get("/", (req, res) => {
   res.status(200).send("Welcome");
 });
 
-// check if email already exists
-app.post("/api/v1/auth/signup", (req, res, next) => {
-  const { email } = req.body;
-  const sql = "SELECT * FROM users WHERE email = ?";
-  db.query(sql, [email], (err, results) => {
-    if (err) {
-      return serverError(res, err);
-    }
-    if (results.length === 0) {
-      next();
-    } else {
-      return clientError(res, "Email already exists");
-    }
-  });
-});
-
 // Omega just cloned and made hs first commit
-app.post("/create", validateUserEmail, checkIfUserAlreadyExist, (req, res) => {
-  const { email, password } = req.body;
-  const uniqueID = generateUniqueUserId();
-  try {
-    const newUser = `INSERT INTO users (email, password) VALUES ('${email}', '${password}')`;
-    db.query(newUser, (err, result) => {
-      if (err) {
-        clientError(res, err);
-      } else {
-        res.status(200).json({
-          success: true,
-          message: "User created",
-          data: result,
-        });
-      }
-    });
-  } catch (error) {
-    console.log(error);
-    serverError(res, error);
-  }
-});
+// app.post("/create", validateUserEmail, checkIfUserAlreadyExist, (req, res) => {
+//   const { email, password } = req.body;
+//   const uniqueID = generateUniqueUserId();
+//   try {
+//     const newUser = `INSERT INTO users (email, password) VALUES ('${email}', '${password}')`;
+//     db.query(newUser, (err, result) => {
+//       if (err) {
+//         clientError(res, err);
+//       } else {
+//         res.status(200).json({
+//           success: true,
+//           message: "User created",
+//           data: result,
+//         });
+//       }
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     serverError(res, error);
+//   }
+// });
 
 // get all users
-// @desc: get all users from users tabel || @route: GET /api/get/users  || @access:admin
+// @desc: get all users from users table || @route: GET /api/get/users  || @access:admin
 app.get("/users", (req, res) => {
   try {
     const getUsers = "SELECT * FROM users";
@@ -86,8 +70,6 @@ app.get("/users", (req, res) => {
     serverError(res, error);
   }
 });
-
-console.log(generateUniqueUserId());
 
 const signUpRoute = require('./routes/users/signup')
 const userActionRoute = require('./routes/users/actions')
